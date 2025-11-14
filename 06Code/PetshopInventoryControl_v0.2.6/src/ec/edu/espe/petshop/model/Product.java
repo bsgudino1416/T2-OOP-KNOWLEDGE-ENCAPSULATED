feeb6913f4aa377bb8e60375ef534e65844e29ff
Mikael <jmhidalgo7@espe.edu.ec>
2025-11-14 00:49:24 -0500

    v0.2.6 petshopInventory Control

---
+++ 06Code/PetshopInventoryControl_v0.2.6/src/ec/edu/espe/petshop/model/Product.java
@@ -0,0 +1,73 @@
+
+package ec.edu.espe.petshop.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+    private String category;
+
+    // Constructor con ID
+    public Product(int id, String name, double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    // Constructor con ID y categoría
+    public Product(int id, String name, double price, int stock, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | " + name + " | $" + price + " | Stock: " + stock;
+    }
+}
